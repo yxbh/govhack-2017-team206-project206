7f1271f1a5056f96252df5dc7e4050bc5f7dcd54
Simon Victory <email@simonvictory.com>
2017-07-30 13:11:13 +1000

    fix python script paths and json

--- Tools/convert-cameras-to-geojson.py
+++ Tools/convert-cameras-to-geojson.py
@@ -1,13 +1,17 @@
 import csv
+import os
 import json
 from string import Template
 
 fieldnames = ("CameraType","LocationDesc","SumPenAmt","Latitude", "Longitude")
+dir = os.path.dirname(__file__)
+trafficdatafile = os.path.dirname("traffic_join_output.csv")
+cameradatafile = os.path.dirname("cameras.geojson")
 
-with open(r'../Resources/traffic_join_output.csv', 'r') as csvfile, open(r'../Resources/cameras.json', 'w') as jsonfile:
+with open(os.path.join(dir, '..', 'Resources', 'traffic_join_output.csv'), 'r') as csvfile, open(os.path.join(dir, '..', 'Resources', 'cameras.geojson'), 'w') as jsonfile:
     reader = csv.DictReader(csvfile) #, fieldnames)
     count = 0
-    json.dump(json.JSONDecoder().decode("""{"type": "FeatureCollection","copyright": "This data has been transformed from the dataset ACT Transport datasets: https://www.data.act.gov.au/d/2sx9-4wg7 and https://www.data.act.gov.au/d/426s-vdu4 for a Canberra GovHack2017 entry","timestamp": "2017-07-30T02:50:01Z","features": ["""), jsonfile, indent=4)
+    jsonfile.write("""{"type": "FeatureCollection","copyright": "This data has been transformed from the dataset ACT Transport datasets: https://www.data.act.gov.au/d/2sx9-4wg7 and https://www.data.act.gov.au/d/426s-vdu4 for a Canberra GovHack2017 entry","timestamp": "2017-07-30T02:50:01Z","features": [""")
     firstrun = True
     for row in reader:
         count+=1    
@@ -23,8 +27,11 @@ with open(r'../Resources/traffic_join_output.csv', 'r') as csvfile, open(r'../Re
             continue
 
         if not firstrun:
-            json.dump(",\n")
-        jsonTemplate = Template("""{"type": "Feature","properties": {"SumPenAmt": "$SumPenAmt", "CameraType": "$CameraType", "LocationDesc": "$LocationDesc"}, "geometry": {"type": "Point", "coordinates": [$Latitude,$Longitude]}}}""")
+            jsonfile.write(",\n")
+        else:
+            firstrun = False
+
+        jsonTemplate = Template("""{"type": "Feature","properties": {"SumPenAmt": "$SumPenAmt", "CameraType": "$CameraType", "LocationDesc": "$LocationDesc"}, "geometry": {"type": "Point", "coordinates": [$Latitude,$Longitude]}}""")
         jsonTemplate = jsonTemplate.substitute(SumPenAmt=row['SumPenAmt'], CameraType=row['CameraType'], LocationDesc=row['LocationDesc'], Latitude=row['Latitude'], Longitude=row['Longitude'])   
         
         json.dump(json.JSONDecoder().decode(jsonTemplate), jsonfile, indent=4)
@@ -35,4 +42,4 @@ with open(r'../Resources/traffic_join_output.csv', 'r') as csvfile, open(r'../Re
         if (count > 1000):
             break
     
-    json.dump("]}")
+    jsonfile.write("]}")
